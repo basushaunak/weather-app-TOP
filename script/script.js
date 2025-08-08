b206a64fcac7ddc2a6daa7558d23034ab6fe9234
Shaunak Basu <55021457+basushaunak@users.noreply.github.com>
2025-08-08 20:47:20 +0530

    WIP

--- script/script.js
+++ script/script.js
@@ -2,4 +2,8 @@
 
 // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 // https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-    // https://openweathermap.org/img/wn/{icon_code}@2x.png
+// https://openweathermap.org/img/wn/{icon_code}@2x.png
+
+window.onload = () => {
+  document.querySelector("#get-city-dialog").showModal();
+};
