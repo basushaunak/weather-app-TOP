76515641a23364d247bc740e004a1995af3831e7
Shaunak Basu <55021457+basushaunak@users.noreply.github.com>
2025-08-08 08:00:19 +0530

    WIP

--- script/script.js
+++ script/script.js
@@ -1,1 +1,4 @@
 // JS Scripts
+
+// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+// https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
